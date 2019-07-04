3c02230e656b8c8e1fb0b5ad96a7b23114a284ef
Jaakko Huusko <jaakko.huusko@ramboll.fi>
2019-07-04 16:52:41 +0300

    Added class for discrete frame optimization problems

--- src/optimization/benchmark_trusses.py
+++ src/optimization/benchmark_trusses.py
@@ -1,6 +1,49 @@
 from optimization.structopt import *
 from frame2d.frame2d import *
-from optimization.optsolver import DiscreteVNS
+from optimization.optsolver import DiscreteVNS, BNB
+from sections.steel.catalogue import rhs_profiles, ipe_profiles
+from truss2d import Truss2D
+
+RHS_PROFILES = list(rhs_profiles.keys())
+# Sorted by area
+RHS_PROFILES = ['RHS 40X40X2.0', 'RHS 40X40X2.5', 'RHS 50X50X2.0',
+                'RHS 40X40X3.0', 'RHS 60X60X2.0', 'RHS 50X50X2.5',
+                'RHS 70X70X2.0', 'RHS 50X50X3.0', 'RHS 60X60X2.5',
+                'RHS 80X80X2.0', 'RHS 70X70X2.5', 'RHS 60X60X3.0',
+                'RHS 50X50X4.0', 'RHS 80X80X2.5', 'RHS 70X70X3.0', 
+                'RHS 60X60X4.0', 'RHS 90X90X2.5', 'RHS 80X80X3.0',
+                'RHS 100X100X2.5', 'RHS 70X70X4.0', 'RHS 90X90X3.0', 
+                'RHS 60X60X5.0', 'RHS 100X100X3.0', 'RHS 120X120X2.5',
+                'RHS 80X80X4.0', 'RHS 70X70X5.0', 'RHS 90X90X4.0',
+                'RHS 140X140X2.5', 'RHS 120X120X3.0', 'RHS 80X80X5.0',
+                'RHS 150X150X2.5', 'RHS 100X100X4.0', 'RHS 140X140X3.0',
+                'RHS 90X90X5.0', 'RHS 150X150X3.0', 'RHS 120X120X4.0',
+                'RHS 100X100X5.0', 'RHS 160X160X3.0', 'RHS 140X140X4.0',
+                'RHS 100X100X6.0', 'RHS 120X120X5.0', 'RHS 150X150X4.0',
+                'RHS 160X160X4.0', 'RHS 140X140X5.0', 'RHS 120X120X6.0',
+                'RHS 180X180X4.0', 'RHS 150X150X5.0', 'RHS 120X120X7.1',
+                'RHS 160X160X5.0', 'RHS 200X200X4.0', 'RHS 140X140X6.0',
+                'RHS 150X150X6.0', 'RHS 120X120X8.0', 'RHS 180X180X5.0',
+                'RHS 160X160X6.0', 'RHS 120X120X8.8', 'RHS 180X180X5.6',
+                'RHS 200X200X5.0', 'RHS 150X150X7.1', 'RHS 120X120X10.0',
+                'RHS 180X180X6.0', 'RHS 160X160X7.1', 'RHS 220X220X5.0',
+                'RHS 150X150X8.0', 'RHS 200X200X6.0', 'RHS 160X160X8.0',
+                'RHS 150X150X8.8', 'RHS 180X180X7.1', 'RHS 250X250X5.0',
+                'RHS 220X220X6.0', 'RHS 160X160X8.8', 'RHS 150X150X10.0',
+                'RHS 180X180X8.0', 'RHS 200X200X7.1', 'RHS 160X160X10.0',
+                'RHS 180X180X8.8', 'RHS 250X250X6.0', 'RHS 300X300X5.0', 
+                'RHS 220X220X7.1', 'RHS 200X200X8.0', 'RHS 260X260X6.0',
+                'RHS 180X180X10.0', 'RHS 200X200X8.8', 'RHS 220X220X8.0',
+                'RHS 250X250X7.1', 'RHS 300X300X6.0', 'RHS 260X260X7.1', 
+                'RHS 220X220X8.8', 'RHS 200X200X10.0', 'RHS 250X250X8.0', 
+                'RHS 180X180X12.5', 'RHS 260X260X8.0', 'RHS 220X220X10.0',
+                'RHS 300X300X7.1', 'RHS 250X250X8.8', 'RHS 260X260X8.8',
+                'RHS 200X200X12.5', 'RHS 300X300X8.0', 'RHS 250X250X10.0',
+                'RHS 400X400X6.0', 'RHS 260X260X10.0', 'RHS 300X300X8.8',
+                'RHS 400X400X7.1', 'RHS 250X250X12.5', 'RHS 300X300X10.0',
+                'RHS 260X260X12.5', 'RHS 400X400X8.0', 'RHS 400X400X8.8',
+                'RHS 300X300X12.5', 'RHS 400X400X10.0', 'RHS 400X400X12.5']
+IPE_PROFILES = list(ipe_profiles.keys())
 
 ten_bar_DLM = [41, 0, 38, 31, 0, 0, 27, 38, 37, 0]
 ten_bar_PSO = [40, 0, 38, 29, 0, 0, 27, 39, 37, 1]
@@ -35,6 +78,98 @@ FIFTEEN_BAR_AREAS_mm2 = [71.613, 90.968, 112.258, 141.935, 174.193, 185.161, 223
                          5999.988, 6999.986, 8580.628, 9219.336, 11077.397, 12374.169]
 
 
+
+class DiscreteFrame(OptimizationProblem):
+    """
+    General class for creating optimization problems that utilize the Frame2D -module
+    
+    Finite element analysis is done in the objective function and it's should always
+    be calculated before constraints.
+
+    """
+    def __init__(self,name="Discrete Frame", profiles=RHS_PROFILES):
+        super().__init__(name)
+
+        self.profiles = profiles
+        
+    
+    def add_structure(self, structure):
+        self.structure = structure
+        self.create_variables()
+        self.create_objective()
+    
+    
+    def create_variables(self):
+        self.vars = []
+        for i in range(len(self.structure.members)):
+            X = IntegerVariable('X' + str(i+1), 0, len(self.profiles) - 1)
+            self.vars.append(X)
+    
+    
+    def create_objective(self):
+
+        def objective(X):
+            if isinstance(X[0], IntegerVariable):
+                X = [x.value for x in X]
+
+            weight = 0
+            for x, mem in zip(X, self.structure.members.values()):
+                x = int(x)
+                mem.profile = self.profiles[x]
+                weight += mem.weight
+            # Calculate stucture with new values
+            self.structure.calculate()
+            return weight
+
+        self.obj = objective
+    
+    
+    def _create_normal_force_constraints(self):
+        """
+        Creates normal force constraints
+        """
+        for i in range(len(self.structure.members)):
+            def con_fun(X, i=i):
+                mem = self.structure.members[i]
+                return mem.ned / mem.NRd - 1
+            constr = NonLinearConstraint(con_fun, name='Normal Force ' + str(i+1))
+            self.cons.append(constr)
+    
+    
+    def _create_shear_force_constraints(self):
+        """
+        Creates shear force constraints
+        """
+        for i in range(len(self.structure.members)):
+            def con_fun(X, i=i):
+                mem = self.structure.members[i]
+                return mem.ved / mem.VRd - 1
+            constr = NonLinearConstraint(con_fun, name='Shear Force ' + str(i+1))
+            self.cons.append(constr)
+            
+            
+    def _create_bending_moment_constraints(self):
+        """
+        Creates bending moment constraints about y-axis
+        """
+        for i in range(len(self.structure.members)):
+            def con_fun(X, i=i):
+                mem = self.structure.members[i]
+                return mem.med / mem.MRd[0] - 1
+            constr = NonLinearConstraint(con_fun, name='Bending Moment ' + str(i+1))
+            self.cons.append(constr)
+    
+    def create_constraints(self, N=True, V=True, M=True):
+
+        # Initialize constraints as an empty list
+        self.cons = []
+        # Create normal force constraints
+        if N:
+            self._create_normal_force_constraints()
+        if V:
+            self._create_shear_force_constraints()
+        if M: 
+            self._create_bending_moment_constraints()
 
 
 class ThreeBarTruss(OptimizationProblem):
@@ -314,9 +449,38 @@ class TenBarTruss(OptimizationProblem):
 
         self.obj = objective
 
+
 if __name__ == '__main__':
+    
+    frame = Frame2D(simple=[1, 1, 4000, 15000],
+                    supports='fixed',
+                    beams=False,
+                    num_elements=2)
+    truss = Truss2D(simple={'H0': 4000,
+                            'H1': 300,
+                            'H2': 600,
+                            'L1': 7500,
+                            'n': 16},
+                    fem_model=frame.f)
+    frame.add(truss)
+    frame.add(LineLoad(truss.members['T0'], [-50, -50], 'y'))
+    frame.add(LineLoad(truss.members['T1'], [-50, -50], 'y'))
+    frame.generate()
+    frame.calculate()
+    problem = DiscreteFrame(profiles=RHS_PROFILES)
+    problem.add_structure(frame)
+    problem.create_constraints(V=False, M=False)
+    frame.plot()
     #problem = TenBarTruss()
-    problem = FifteenBarTruss()
-    solver = DiscreteVNS(step_length=3)
-    solver.solve(problem, maxiter=int(1e3), maxtime=300)
+    #problem.structure.plot()
+    #problem = FifteenBarTruss()
+    sub_size = int(len(frame.members)/4)
+    solver = DiscreteVNS(step_length=1, subset_size=sub_size)
+    #solver = BNB()
+    #solver.solve(problem)
+    solver.solve(problem,
+                 x0=[67, 74, 76, 85, 49, 93, 66, 103, 97, 85, 96, 101, 103, 96, 90, 57, 89, 105, 96, 90, 99],
+                 maxiter=1000,
+                 maxtime=60,
+                 verbose=1)
 
