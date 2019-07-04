6def12eef6a37edfb668f15840345259cce02798
Jaakko Huusko <jaakko.huusko@student.tut.fi>
2019-07-04 10:37:06 +0300

    Minor changes to printing and variable numbering

--- src/optimization/structopt.py
+++ src/optimization/structopt.py
@@ -15,11 +15,12 @@ a form suitable for each solver.
 import numpy as np
 import scipy.optimize as sciop
 
+
 class Variable:
-    """ Class for optimization variables     
+    """ Class for optimization variables
     """
-    
-    def __init__(self,name,lb,ub,target=None,profiles=None):
+
+    def __init__(self, name, lb, ub, target=None, profiles=None):
         """
         Parameters:
             -----------
@@ -31,16 +32,16 @@ class Variable:
                 :param target: dict that provides information about the part of
                 the structure that the variable is changing
                 :param profiles: list of available profiles (optional)
-                
+
                 target = {"property": string, "objects": list}
                     property .. unique identifier that shows which property of the
                                 structure is affected by the variable
                     objects .. list of objects (e.g. frame members) that are
                                 affected by the variable
-                
+
                 if target is 'None', then the variable does not change anything
                 in the structure
-                
+
                 Allowed values for 'property':
                     'AREA' .. cross-sectional area of a member
                     'IY' .. second moment of area of a member (major axis)
@@ -61,12 +62,12 @@ class Variable:
         self.ub = ub
         self.target = target
         self.profiles = profiles
-        
-    def substitute(self,new_value):
+
+    def substitute(self, new_value):
         """ Substitute a new value for the variable """
-        
+
         self.value = new_value
-        
+
         """ Modify target object(s) if any """
         if self.target is not None:
             for obj in self.target['objects']:
@@ -86,19 +87,19 @@ class Variable:
                     obj.Wpl[1] = new_value
                 elif self.target['property'] == 'H':
                     obj.h = new_value
-                elif self.target['property'] == 'TW':    
+                elif self.target['property'] == 'TW':
                     obj.tw = new_value
                 elif self.target['property'] == 'BF':
-                    if isinstance(obj.b,list):
+                    if isinstance(obj.b, list):
                         for i in range(len(obj.b)):
                             obj.b[i] = new_value
-                    else:                        
+                    else:
                         obj.b = new_value
-                elif self.target['property'] == 'TF':    
-                    if isinstance(obj.tf,list):
+                elif self.target['property'] == 'TF':
+                    if isinstance(obj.tf, list):
                         for i in range(len(obj.b)):
                             obj.tf[i] = new_value
-                    else:                        
+                    else:
                         obj.tf = new_value
                 elif self.target['property'] == 'PROFILE':
                     """ susbstitute profile """
@@ -106,122 +107,127 @@ class Variable:
                         obj.profile = self.profiles[new_value]
                     except ValueError:
                         print('Index variable value must be integer!')
-                    
-        
+
+
 class IntegerVariable(Variable):
-    """ Class for integer variables 
-    
+    """ Class for integer variables
+
         Integer variables can take integer values from the interval lb, ub
     """
-    
-    def __init__(self,name="",lb=0,ub=1e5):
+
+    def __init__(self, name="", lb=0, ub=1e5):
         """ Constructor
-            
+
             Arguments:
                 name .. string stating the name of the variable
                 lb .. lower bound
                 ub .. upper bound
         """
 
-        Variable.__init__(self,name,lb,ub)
-        
+        Variable.__init__(self, name, lb, ub)
+
+
 class BinaryVariable(IntegerVariable):
-    """ Class for binary variables 
-        
+    """ Class for binary variables
+
         Binary variables can only have values 0 or 1
-    
+
     """
-    
-    def __init__(self,name=""):
+
+    def __init__(self, name=""):
         """ Constructor
-            
+
             Arguments:
                 name .. string stating the name of the variable
         """
-        
-        IntegerVariable.__init__(self,name,0,1)
-        
+
+        IntegerVariable.__init__(self, name, 0, 1)
+
+
 class DiscreteVariable(Variable):
     """ Class for general discrete variables """
-    
-    def __init__(self,name="",values=None):
+
+    def __init__(self, name="", values=None):
         """ Constructor
-            
+
             Parameters:
             -----------
                 :param name: string stating the name of the variable
                 :param values: list of allowable discrete values
-                
+
                 Variables:
                 ----------
                 :ivar values: list of allowable discrete values
                 :ivar name: name of the variable (string)
         """
-        
+
         self.values = values
-        
+
         if values != None:
             lb = min(values)
             ub = max(values)
         else:
             lb = None
             ub = None
-        
-        Variable.__init__(self,name,lb,ub)
-        
+
+        Variable.__init__(self, name, lb, ub)
+
+
 class Constraint:
     """ General class for constraints """
-    
-    def __init__(self,name="",con_type="<"):
-                
+
+    def __init__(self, name="", con_type="<"):
         self.name = name
         self.type = con_type
+
 
 class LinearConstraint(Constraint):
     """ Class for linear constraints """
 
-    def __init__(self,a=None,b=None,con_type="<",name=""):
+    def __init__(self, a=None, b=None, con_type="<", name=""):
         """ Constructor """
-        
+
         self.a = a
         self.b = b
-        Constraint.__init__(self,name,con_type)
-        
-    def __call__(self,x):
-        """ Evaluate constraint at x """    
-        return np.array(self.a).dot(np.array(x))-self.b
-    
-    def neg_call(self,x):
+        Constraint.__init__(self, name, con_type)
+
+    def __call__(self, x):
+        """ Evaluate constraint at x """
+        return np.array(self.a).dot(np.array(x)) - self.b
+
+    def neg_call(self, x):
         """ Evaluate -g(x) """
         return -self(x)
+
 
 class NonLinearConstraint(Constraint):
     """ Class for linear constraints """
 
-    def __init__(self,con_fun,con_type="<",name=""):
-        """ Constructor 
+    def __init__(self, con_fun, con_type="<", name=""):
+        """ Constructor
             con_fun: function that returns the value of constraint function
                      g(x) <= 0 (or >= 0 or = 0)
         """
-        
+
         self.con = con_fun
-        Constraint.__init__(self,name,con_type)
-    
-    def __call__(self,x):
-        """ Evaluate constraint at x """        
+        Constraint.__init__(self, name, con_type)
+
+    def __call__(self, x):
+        """ Evaluate constraint at x """
         return self.con(x)
-    
-    def neg_call(self,x):
+
+    def neg_call(self, x):
         """ Return -g(x) """
         return -self.con(x)
-    
+
+
 class OptimizationProblem:
     """ Class for defining and operating with optimization problems """
-    
-    def __init__(self,name="",variables=[],constraints=[],objective=None,\
-                 gradient=None,hess=None, structure=None, profiles=None):
+
+    def __init__(self, name="", variables=[], constraints=[], objective=None, \
+                 gradient=None, hess=None, structure=None, profiles=None):
         """ Constructor
-            
+
             Parameters:
             -----------
                 :param name: name of the problem (string)
@@ -231,8 +237,8 @@ class OptimizationProblem:
                 :param gradient: gradient of the objective function
                 :param hess: Hessian of the objective function
                 :param structure: structure to be optimized (optional)
-                :param profiles: list of available profiles (optional)                        
-                
+                :param profiles: list of available profiles (optional)
+
                 Variables:
                 ----------
                 :ivar name: name of the problem
@@ -245,7 +251,7 @@ class OptimizationProblem:
                 :ivar profiles: list of available profiles (optional)
         """
 
-        self.name = name        
+        self.name = name
         self.vars = variables
         self.cons = constraints
         self.obj = objective
@@ -253,64 +259,63 @@ class OptimizationProblem:
         self.hess = hess
         self.structure = structure
         self.profiles = profiles
-        
-    def __call__(self,x):
+
+    def __call__(self, x):
         """ Call method evaluates the objective function and all constraints
             at x and returns their values
         """
-        
+
         fx = self.obj(x)
         print("** {0} **".format(self.name))
         if len(x) < 10:
             print("Variables:")
             print("----------")
             for i in range(len(x)):
-                print("{0} = {1:4.2f}".format(self.vars[i].name,x[i]))
-            
-            print("----------\n")    
-            
+                print("{0} = {1:4.2f}".format(self.vars[i].name, x[i]))
+
+            print("----------\n")
+
         print("Objective function = {0:4.2f}\n".format(fx))
-        
+
         print("Constraints:")
         print("----------")
-        
+
         for con in self.cons:
-            g = con(x)            
-            print("{0}: {1:4.2f} {2} 0".format(con.name,g,con.type))
-        
-    def eval_con(self,x):
+            g = con(x)
+            print("{0}: {1:4.2f} {2} 0".format(con.name, g, con.type))
+
+    def eval_con(self, x):
         """ Constraint evaluation
-            
+
         """
         g = []
-        
+
         for con in self.cons:
             g.append(con(x))
-        
-        return g        
-    
-    def eval_eq_con(self,x):
+
+        return g
+
+    def eval_eq_con(self, x):
         """ Evaluates equality constraionts """
-        
+
         geq = []
-        
+
         for con in self.cons:
             if con.type == "=":
                 geq.append(con(x))
-    
-    def eval_ineq_con(self,x):
+
+    def eval_ineq_con(self, x):
         """ Evaluate inequality constriants
             All constraints are evaluated in the form g(x) >= 0
         """
-        
+
         g = []
-        
+
         for con in self.cons:
             if con.type == ">":
                 g.append(con(x))
             elif con.type == "<":
                 g.append(-con(x))
-                
 
     def nvars(self):
         """ Return number of variables """
@@ -324,35 +329,35 @@ class OptimizationProblem:
         """ Number of nonlinear equality constraints """
         return len(self.vars)
 
-    def add_variables(self,variables):
+    def add_variables(self, variables):
         """ Adds new variables to the problem """
-        
+
         self.vars.append(variables)
-        
-    def add_constraints(self,constraints):
+
+    def add_constraints(self, constraints):
         """ Adds new constraints to the problem """
         self.cons.append(constraints)
-    
-    def add_structure(self,structure):
+
+    def add_structure(self, structure):
         """ Add structure to the problem """
         self.structure = structure
-    
+
     def var_bounds(self):
         """ Returns arrays lb and ub for variable bounds """
         lb = []
         ub = []
-            
+
         for v in self.vars:
             lb.append(v.lb)
             ub.append(v.ub)
-        
+
         return lb, ub
-    
-    def substitute_variable(self,i,xval):
+
+    def substitute_variable(self, i, xval):
         """ Substitutes the value 'xval' for variable i """
         var = self.vars[i]
         var.value = xval
-        
+
         if var.target is not None:
             for obj in var.target['objects']:
                 if var.target['property'] == 'AREA':
@@ -371,43 +376,39 @@ class OptimizationProblem:
                     obj.Wpl[1] = xval
                 elif var.target['property'] == 'H':
                     obj.h = xval
-                elif var.target['property'] == 'TW':    
+                elif var.target['property'] == 'TW':
                     obj.tw = xval
-                elif var.target['property'] == 'BF':    
+                elif var.target['property'] == 'BF':
                     obj.b = xval
-                elif var.target['property'] == 'TF':    
+                elif var.target['property'] == 'TF':
                     obj.tf = xval
                 elif var.target['property'] == 'PROFILE':
                     obj.profile = self.profiles(xval)
-                    
-    
-    
-    def substitute_variables(self,xvals):
+
+    def substitute_variables(self, xvals):
         """ Substitute variable values from xval to structure """
-        
+
         for i in range(len(xvals)):
-            self.substitute_variable(i,xvals[i])
-            
-    
-    def solve(self,solver="slsqp",**kwargs):
-        """ Solve the optimization problem 
+            self.substitute_variable(i, xvals[i])
+
+    def solve(self, solver="slsqp", **kwargs):
+        """ Solve the optimization problem
             input:
                 solver .. name of the solver
                 **kwargs .. list of keyword argument
         """
         lb, ub = self.var_bounds()
-                
-        
+
         if solver == "slsqp":
-            jac = "2-point"            
-            bnds = sciop.Bounds(lb,ub)        
-            
+            jac = "2-point"
+            bnds = sciop.Bounds(lb, ub)
+
             if self.grad is not None:
                 jac = self.grad
-            
+
             if self.hess is not None:
-                hess = self.hess      
-                
+                hess = self.hess
+
             """ Crerate constraints
             Constraints for COBYLA, SLSQP are defined as a 
             list of dictionaries. Each dictionary with fields:
@@ -425,96 +426,94 @@ class OptimizationProblem:
                     Extra arguments to be passed to the function and Jacobian.
 
             """
-            #cons = [{"type":"ineq", "fun":self.eval_ineq_con}]
-            
-            
+            # cons = [{"type":"ineq", "fun":self.eval_ineq_con}]
+
+
             cons = []
             for con in self.cons:
                 if con.type == "=":
-                    new_con = {"type":"eq", "fun":con}
+                    new_con = {"type": "eq", "fun": con}
                 elif con.type == "<":
-                    new_con = {"type":"ineq", "fun":con.neg_call}
+                    new_con = {"type": "ineq", "fun": con.neg_call}
                 else:
-                    new_con = {"type":"ineq", "fun":con}
-                    
+                    new_con = {"type": "ineq", "fun": con}
+
                 cons.append(new_con)
-            
-            #print(cons)
-            
-            res = sciop.minimize(self.obj,kwargs["x0"],method=solver,\
-                           jac=jac,bounds=bnds,constraints=cons)
+
+            # print(cons)
+
+            res = sciop.minimize(self.obj, kwargs["x0"], method=solver, \
+                                 jac=jac, bounds=bnds, constraints=cons)
         elif solver == "trust-constr":
-            jac = "2-point"        
+            jac = "2-point"
             hess = "2-point"
-            bnds = sciop.Bounds(lb,ub)            
-            
+            bnds = sciop.Bounds(lb, ub)
+
             if self.grad is not None:
                 jac = self.grad
-            
-            if self.hess is not None:
-                hess = self.hess          
 
-            res = sciop.minimize(self.obj,kwargs["x0"],method=solver,\
-                           jac=jac,hess=hess,bounds=bnds)
+            if self.hess is not None:
+                hess = self.hess
+
+            res = sciop.minimize(self.obj, kwargs["x0"], method=solver, \
+                                 jac=jac, hess=hess, bounds=bnds)
         else:
             print("Solver unidentified.")
             res = None
-        
+
         return res
 
 
-def NumGrad(fun,h,x):
+def NumGrad(fun, h, x):
     """ Gradient by finite difference """
     fx = fun(x)
     n = len(x)
-   
+
     df = np.zeros(n)
-    
+
     for i in range(len(x)):
         dx = np.zeros(n)
         dx[i] = h[i]
-        print(x+dx)
-        fh = fun(x+dx)        
-        df[i] = (fh-fx)/h[i]
+        print(x + dx)
+        fh = fun(x + dx)
+        df[i] = (fh - fx) / h[i]
 
     return df
 
-def Linearize(fun,x,grad=None):
-    """ Linearization of a function 
-    
+
+def Linearize(fun, x, grad=None):
+    """ Linearization of a function
+
         input:
             fun .. function that takes x as input
             x .. point of linearization (array)
             grad .. returns the gradient of fun at x
-            
+
         output:
             a .. grad(x)
             b .. fun(x)
-            c .. grad(x)*x 
-            
+            c .. grad(x)*x
+
     """
-    
+
     b = fun(x)
-    
+
     if grad == None:
-        h = 0.01*x # check if this is OK
-        a = NumGrad(fun,h,x)
+        h = 0.01 * x  # check if this is OK
+        a = NumGrad(fun, h, x)
     else:
         a = grad(x)
-    
+
     c = a.dot(x)
-    
+
     return a, b, c
 
 
-
 if __name__ == '__main__':
-    
     dvars = []
-    dvars.append(Variable("Height",150,1000))
-    dvars.append(Variable("Flange width",150,500))
-    dvars.append(Variable("Flange thickness",5,40))
-    dvars.append(Variable("Web thickness",5,40))
-    
-    p = OptimizationProblem(name="I-Beam Weight Minimization",variables=dvars)
-    
+    dvars.append(Variable("Height", 150, 1000))
+    dvars.append(Variable("Flange width", 150, 500))
+    dvars.append(Variable("Flange thickness", 5, 40))
+    dvars.append(Variable("Web thickness", 5, 40))
+
+    p = OptimizationProblem(name="I-Beam Weight Minimization", variables=dvars)
