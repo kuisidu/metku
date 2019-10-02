8cd1f6f97bcc3ee7ab26dfd440dc5d04f19a8ab7
Jaakko Huusko <jaakko.huusko@student.tut.fi>
2019-10-02 19:28:44 +0300

    Added steel materials

--- src/frame2d/materials.py
+++ src/frame2d/materials.py
@@ -19,10 +19,14 @@ MATERIALS = dict(
     S355=Steel(355, 510, "S355"),
     S280GD=Steel(280, 360, "S280GD"),
     S420=Steel(420, 500, "S420"),
+    S355MC=Steel(355.0, 430.0, "S355MC"),
+    S500MC=Steel(500.0, 550.0, "S500MC"),
+    S700MC=Steel(700.0, 750.0, "S700MC"),
+    S355ML=Steel(355.0, 470.0, "S355ML"),
+    S500ML=Steel(500.0, 570.0, "S500ML"),
+
 )
 
-
-
-
+MATERIALS["S700E/F"] = Steel(700.0, 780.0, "S700E/F")
 
 
