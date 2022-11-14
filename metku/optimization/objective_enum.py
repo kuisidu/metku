3ce6d04001d7bdd35bd707ab2b9a4649c40a5b41
HuuskoJ <huusko.jaakko@outlook.com>
2022-11-14 20:36:04 +0200

    Added ObjectiveEnum -class (objective_enum.py)

---
+++ metku/optimization/objective_enum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+#  Copyright (c) 2022. Metku team.
+#  All rights reserved.
+
+from enum import Enum
+
+
+class ObjectiveEnum(Enum):
+    WEIGHT = 1
+    COST = 2
