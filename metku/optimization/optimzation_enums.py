5294df83e2a5dfa4e70935c5e84cd2d60da1cee7
huuskoj <huusko.jaakko@outlook.com>
2024-08-30 10:27:47 +0300

    Changed enum type to StrEnum

--- metku/optimization/optimzation_enums.py
+++ metku/optimization/optimzation_enums.py
@@ -5,8 +5,11 @@
 #  Copyright (c) 2022. Metku team.
 #  All rights reserved.
 
-from enum import Enum
+from enum import Enum, auto, StrEnum
 
+class ObjectiveTypeEnum(StrEnum):
+    MIN = auto()
+    MAX = auto()
 
 class ObjectiveEnum(Enum):
     WEIGHT = 1
